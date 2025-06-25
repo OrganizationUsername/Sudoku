188314485c41a8b6433c8a85b3d3bd52448f9436
Kyouyama Kazusa <Kazusa0805@outlook.com>
2025-06-25 17:34:25 +0800

    Update code.

--- src/Sudoku.Analytics/Ranking/RankPattern.toString.cs
+++ src/Sudoku.Analytics/Ranking/RankPattern.toString.cs
@@ -3,7 +3,18 @@ namespace Sudoku.Ranking;
 public partial struct RankPattern
 {
 	/// <inheritdoc cref="object.ToString"/>
-	public override string ToString() => $"T{Truths.Count} = {Truths}, L{Links.Count} = {Links}";
+	public override string ToString()
+	{
+		var virtualTruthsString = string.Join(' ', from truth in VirtualTruths select truth.Candidates.ToString());
+		var virtualLinksString = string.Join(' ', from link in VirtualLinks select link.Candidates.ToString());
+		return (virtualTruthsString.Length, virtualLinksString.Length) switch
+		{
+			(0, 0) => $"T{Truths.Count} = {Truths}, L{Links.Count} = {Links}",
+			(_, 0) => $"T{Truths.Count} = {Truths}, L{Links.Count} = {Links}, VT = {virtualTruthsString}",
+			(0, _) => $"T{Truths.Count} = {Truths}, L{Links.Count} = {Links}, VL = {virtualLinksString}",
+			_ => $"T{Truths.Count} = {Truths}, L{Links.Count} = {Links}, VT = {virtualTruthsString}, VL = {virtualLinksString}"
+		};
+	}
 
 	/// <summary>
 	/// Gets the full string of the current pattern, including its details (rank, eliminations and so on).

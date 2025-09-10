66428d36748bfa7fa16b38b752d95ab09a9495b1
Kyouyama Kazusa <Kazusa0805@outlook.com>
2025-09-10 17:13:26 +0800

    Sync .NET version.

--- src/SudokuStudio/Resources/SudokuStudioResources.Designer.cs
+++ src/SudokuStudio/Resources/SudokuStudioResources.Designer.cs
@@ -19,7 +19,7 @@ namespace SudokuStudio.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class SudokuStudioResources {
@@ -412,7 +412,7 @@ namespace SudokuStudio.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to This application is supported by .NET 8 runtime, with Windows App SDK..
+        ///   Looks up a localized string similar to This application is supported by .NET 10 runtime, with Windows App SDK..
         /// </summary>
         internal static string AboutPage_FrameworkDescription {
             get {

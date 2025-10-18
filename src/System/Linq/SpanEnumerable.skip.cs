c335e77e86d072bb2210964268f9c042e0989b01
Kyouyama Kazusa <Kazusa0805@outlook.com>
2025-10-18 10:09:23 +0800

    Fix `Skip` behavior.

--- src/System/Linq/SpanEnumerable.skip.cs
+++ src/System/Linq/SpanEnumerable.skip.cs
@@ -10,6 +10,6 @@ public static partial class SpanEnumerable
 	extension<TSource>(ReadOnlySpan<TSource> source)
 	{
 		/// <inheritdoc cref="ISkipMethod{TSelf, TSource}.Skip(int)"/>
-		public ReadOnlySpan<TSource> Skip(int count) => source[count..];
+		public ReadOnlySpan<TSource> Skip(int count) => source.Length < count ? [] : source[count..];
 	}
 }

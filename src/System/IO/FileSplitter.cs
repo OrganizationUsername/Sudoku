d3957a08e3abbfbcb2fd47a6b901639494f92e67
Kyouyama Kazusa <Kazusa0805@outlook.com>
2025-10-20 16:40:48 +0800

    Update code.

--- src/System/IO/FileSplitter.cs
+++ src/System/IO/FileSplitter.cs
@@ -6,64 +6,70 @@ namespace System.IO;
 public static class FileSplitter
 {
 	/// <summary>
-	/// Split file into multiple parts. Output file names will be <c>&lt;file-name&gt;_&lt;index&gt;.&lt;extension&gt;</c>.
+	/// Provides extension members on <see cref="File"/>.
 	/// </summary>
-	/// <param name="path">Indicates input file path.</param>
-	/// <param name="outputDir">Indicates output folder path.</param>
-	/// <param name="parts">Indicates the desired number of parts.</param>
-	/// <param name="cancellationToken">The cancellation token.</param>
-	/// <returns>A task that can handle asynchronous operation.</returns>
-	/// <exception cref="ArgumentException">Throws when <paramref name="parts"/> is an negative number.</exception>
-	/// <exception cref="OperationCanceledException">Throws when <paramref name="cancellationToken"/> is requested.</exception>
-	public static async Task SplitFileByPartsAsync(string path, string outputDir, int parts, CancellationToken cancellationToken = default)
+	extension(File)
 	{
-		ArgumentOutOfRangeException.Assert(parts >= 0);
-		if (!Directory.Exists(outputDir))
+		/// <summary>
+		/// Split file into multiple parts. Output file names will be <c>&lt;file-name&gt;_&lt;index&gt;.&lt;extension&gt;</c>.
+		/// </summary>
+		/// <param name="path">Indicates input file path.</param>
+		/// <param name="outputDir">Indicates output folder path.</param>
+		/// <param name="parts">Indicates the desired number of parts.</param>
+		/// <param name="cancellationToken">The cancellation token.</param>
+		/// <returns>A task that can handle asynchronous operation.</returns>
+		/// <exception cref="ArgumentException">Throws when <paramref name="parts"/> is an negative number.</exception>
+		/// <exception cref="OperationCanceledException">Throws when <paramref name="cancellationToken"/> is requested.</exception>
+		public static async Task SplitFileByPartsAsync(string path, string outputDir, int parts, CancellationToken cancellationToken = default)
 		{
-			Directory.CreateDirectory(outputDir);
-		}
-
-		// Totals up the number of lines.
-		var totalLines = 0L;
-		using (var sr1 = new StreamReader(path))
-		{
-			while (await sr1.ReadLineAsync(cancellationToken) is not null)
+			ArgumentOutOfRangeException.Assert(parts >= 0);
+			if (!Directory.Exists(outputDir))
 			{
-				totalLines++;
-			}
-		}
-		if (totalLines == 0)
-		{
-			return;
-		}
-
-		// Calculates lines for each file.
-		var baseLines = totalLines / parts;
-		var remainder = totalLines % parts;
-
-		// Write out text.
-		using var sr2 = new StreamReader(path);
-		var (p, q) = Path.GetFileNameAndExtension(path);
-		for (var i = 0; i < parts; i++)
-		{
-			cancellationToken.ThrowIfCancellationRequested();
-
-			var outputPath = Path.Combine(outputDir, $"{p}_{i + 1}{q}");
-			var targetLines = baseLines + (i < remainder ? 1 : 0);
-			if (targetLines == 0)
-			{
-				break;
+				Directory.CreateDirectory(outputDir);
 			}
 
-			await using var sw = new StreamWriter(outputPath);
-			for (var j = 0L; j < targetLines; j++)
+			// Totals up the number of lines.
+			var totalLines = 0L;
+			using (var sr1 = new StreamReader(path))
 			{
-				var line = await sr2.ReadLineAsync(cancellationToken);
-				if (line is null)
+				while (await sr1.ReadLineAsync(cancellationToken) is not null)
+				{
+					totalLines++;
+				}
+			}
+			if (totalLines == 0)
+			{
+				return;
+			}
+
+			// Calculates lines for each file.
+			var baseLines = totalLines / parts;
+			var remainder = totalLines % parts;
+
+			// Write out text.
+			using var sr2 = new StreamReader(path);
+			var (p, q) = Path.GetFileNameAndExtension(path);
+			for (var i = 0; i < parts; i++)
+			{
+				cancellationToken.ThrowIfCancellationRequested();
+
+				var outputPath = Path.Combine(outputDir, $"{p}_{i + 1}{q}");
+				var targetLines = baseLines + (i < remainder ? 1 : 0);
+				if (targetLines == 0)
 				{
 					break;
 				}
-				await sw.WriteLineAsync(line);
+
+				await using var sw = new StreamWriter(outputPath);
+				for (var j = 0L; j < targetLines; j++)
+				{
+					var line = await sr2.ReadLineAsync(cancellationToken);
+					if (line is null)
+					{
+						break;
+					}
+					await sw.WriteLineAsync(line);
+				}
 			}
 		}
 	}

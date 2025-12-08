61ac6e9d299c91e7406b51029eca39b7de8ac02b
Kyouyama Kazusa <Kazusa0805@outlook.com>
2025-12-08 11:51:39 +0800

    Update code.

--- src/Sudoku.Core/Solving/BooleanSatisfiability/CnfFormula.cs
+++ src/Sudoku.Core/Solving/BooleanSatisfiability/CnfFormula.cs
@@ -13,8 +13,35 @@ namespace Sudoku.Solving.BooleanSatisfiability;
 /// For background on CNF, see <see href="https://en.wikipedia.org/wiki/Conjunctive_normal_form">Wikipedia</see>.
 /// </remarks>
 /// <seealso href="https://en.wikipedia.org/wiki/Conjunctive_normal_form">Wikipedia - Conjunctive Normal Form</seealso>
+[DebuggerDisplay($$"""{{{nameof(ToDebuggerDisplayString)}}(60),nq}""")]
 public sealed record CnfFormula(int VariablesCount) : IEnumerable<ReadOnlyMemory<int>>
 {
+	/// <summary>
+	/// Represents character <c>"&#172;"</c>.
+	/// </summary>
+	public const char Not = '\u00AC';
+
+	/// <summary>
+	/// Represents character <c>"&#8744;"</c>.
+	/// </summary>
+	public const char Or = '\u2228';
+
+	/// <summary>
+	/// Represents character <c>"&#8743;"</c>.
+	/// </summary>
+	public const char And = '\u2227';
+
+	/// <summary>
+	/// Represents character <c>"&#8868;"</c>.
+	/// </summary>
+	public const char Top = '\u22A4';
+
+	/// <summary>
+	/// Represents character <c>"&#8869;"</c>.
+	/// </summary>
+	public const char Bottom = '\u22A5';
+
+
 	/// <summary>
 	/// Number of clauses currently stored in the formula.
 	/// </summary>
@@ -42,6 +69,9 @@ public sealed record CnfFormula(int VariablesCount) : IEnumerable<ReadOnlyMemory
 	/// <param name="literals">The literals that form the clause.</param>
 	public void AddClause(ReadOnlyMemory<int> literals) => Clauses.Add(literals);
 
+	/// <inheritdoc/>
+	public override string ToString() => ToDebuggerDisplayString(Array.MaxLength);
+
 	/// <inheritdoc cref="IEnumerable{T}.GetEnumerator"/>
 	public AnonymousSpanEnumerator<ReadOnlyMemory<int>> GetEnumerator() => new(Clauses.AsSpan());
 
@@ -50,4 +80,101 @@ public sealed record CnfFormula(int VariablesCount) : IEnumerable<ReadOnlyMemory
 
 	/// <inheritdoc/>
 	IEnumerator<ReadOnlyMemory<int>> IEnumerable<ReadOnlyMemory<int>>.GetEnumerator() => Clauses.GetEnumerator();
+
+	private string ToDebuggerDisplayString(int cutLength)
+	{
+		if (ClauseCount == 0)
+		{
+			return Top.ToString();
+		}
+
+		var sb = new StringBuilder();
+		for (var i = 0; i < Clauses.Count; i++)
+		{
+			var clause = Clauses[i].Span;
+			if (!appendOrFailed(sb, '('))
+			{
+				goto Return;
+			}
+			if (clause.Length == 0)
+			{
+				if (!appendOrFailed(sb, Bottom))
+				{
+					goto Return;
+				}
+			}
+			else
+			{
+				for (var j = 0; j < clause.Length; j++)
+				{
+					var literal = clause[j];
+					if (literal < 0)
+					{
+						if (!appendOrFailed(sb, Not))
+						{
+							goto Return;
+						}
+					}
+
+					if (!appendOrFailed(sb, getVariableName(Math.Abs(literal))))
+					{
+						goto Return;
+					}
+
+					if (j + 1 < clause.Length)
+					{
+						if (!appendOrFailed(sb, Or))
+						{
+							goto Return;
+						}
+					}
+				}
+			}
+			if (!appendOrFailed(sb, ')'))
+			{
+				goto Return;
+			}
+
+			if (i + 1 < Clauses.Count)
+			{
+				if (!appendOrFailed(sb, And))
+				{
+					goto Return;
+				}
+			}
+		}
+
+	Return:
+		return sb.ToString();
+
+
+		static string getVariableName(int variableIndex)
+		{
+			if (variableIndex <= 0)
+			{
+				return variableIndex.ToString();
+			}
+
+			var sb = new StringBuilder();
+			while (variableIndex > 0)
+			{
+				variableIndex--;
+				sb.Insert(0, (char)('A' + variableIndex % 26));
+				variableIndex /= 26;
+			}
+			return sb.ToString();
+		}
+
+
+		bool appendOrFailed<T>(StringBuilder sb, T c) where T : notnull
+		{
+			if (sb.Length < cutLength)
+			{
+				sb.Append(c.ToString());
+				return true;
+			}
+			sb.Append("...");
+			return false;
+		}
+	}
 }

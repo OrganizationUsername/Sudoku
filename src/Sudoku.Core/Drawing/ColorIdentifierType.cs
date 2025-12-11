1b7bc7dbfb01baadb2a4be9ef48ac9287fd57260
Kyouyama Kazusa <Kazusa0805@outlook.com>
2025-12-11 11:35:52 +0800

    Update code.

---
+++ src/Sudoku.Core/Drawing/ColorIdentifierType.cs
@@ -0,0 +1,22 @@
+namespace Sudoku.Drawing;
+
+/// <summary>
+/// Indicates type of color identifier.
+/// </summary>
+public enum ColorIdentifierType
+{
+	/// <summary>
+	/// Indicates the type is ARGB.
+	/// </summary>
+	Argb,
+
+	/// <summary>
+	/// Indicates the type is aliased item.
+	/// </summary>
+	Alias,
+
+	/// <summary>
+	/// Indicates the type is ID.
+	/// </summary>
+	Id
+}

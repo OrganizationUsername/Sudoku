de718267032f2f24f0f3abd02ce28da5f55216ad
Kyouyama Kazusa <Kazusa0805@outlook.com>
2026-04-08 16:42:25 +0800

    Update code.

--- src/System/Reflection/ExtensionMemberLookup.cs
+++ src/System/Reflection/ExtensionMemberLookup.cs
@@ -37,8 +37,121 @@ public static partial class ExtensionMemberLookup
 	/// <summary>
 	/// Extends <see cref="Type"/> instances.
 	/// </summary>
-	extension(Type type)
+	/// <param name="this">The current instance.</param>
+	extension(Type @this)
 	{
+		/// <summary>
+		/// Indicates all extension containers defined in the current type.
+		/// </summary>
+		public ReadOnlySpan<ExtensionContainerMetadata> DeclaredExtensionContainers
+		{
+			get
+			{
+				if (@this is not { IsStatic: true, MightBeExtension: true })
+				{
+					return [];
+				}
+
+				var result = new List<ExtensionContainerMetadata>();
+
+				// Check extension grouping type, whose name must be started with <see cref="ExtensionGrouperTypeNamePrefix"/>.
+
+				// Iterate on all possible types defined in it.
+				foreach (var extensionGrouperType in @this.GetMembers(DefaultBindingFlags).OfType<Type>())
+				{
+					// The name of this type must be started with <see cref="ExtensionGrouperTypeNamePrefix"/>.
+					if (!extensionGrouperType.Name.StartsWith(ExtensionGrouperTypeNamePrefix))
+					{
+						continue;
+					}
+
+					// This type must be 'sealed'.
+					if (!extensionGrouperType.IsSealed)
+					{
+						continue;
+					}
+
+					// This type must be marked as [Extension].
+					if (!extensionGrouperType.MightBeExtension)
+					{
+						continue;
+					}
+
+					// This type must be marked as [SpecialName].
+					// However, we should check for this type by using <see cref="Type.Attributes"/>,
+					// rather than using <see cref="MemberInfo.IsDefined(Type, bool)"/>.
+					if (!extensionGrouperType.IsSpecialName)
+					{
+						continue;
+					}
+
+					// This type must be non-'static'.
+					if (extensionGrouperType.IsStatic)
+					{
+						continue;
+					}
+
+					// Step 3: Check extension marker type defined in extension grouping type.
+
+					// This type is also a static class.
+					var correspondingExtensionMarkerType = default(Type);
+					foreach (var extensionMarkerType in extensionGrouperType.GetMembers(DefaultBindingFlags).OfType<Type>())
+					{
+						// The name of this type must be started with '<see cref="ExtensionMarkerTypeNamePrefix"/>'.
+						if (!extensionMarkerType.Name.StartsWith(ExtensionMarkerTypeNamePrefix))
+						{
+							continue;
+						}
+
+						// This type must be marked [SpecialName].
+						if (!extensionGrouperType.IsSpecialName)
+						{
+							continue;
+						}
+
+						// This type must be 'static'.
+						if (!extensionMarkerType.IsStatic)
+						{
+							continue;
+						}
+
+						// This type must contain one and only one member - a 'static' method,
+						// with name '<see cref="ExtensionMarkerMethodName"/>'.
+						var possibleMethodsInfo = extensionMarkerType.GetMembers(DefaultBindingFlags)
+							.OfType<MethodInfo>()
+							.ToArray();
+						if (possibleMethodsInfo is not [
+							{
+								IsGenericMethod: false,
+								Name: ExtensionMarkerMethodName,
+								ReturnType: var returnType,
+								IsSpecialName: true,
+								IsCompilerGenerated: true,
+								Parameters: [{ ParameterType: var parameterType }],
+							}]
+							|| returnType != typeof(void))
+						{
+							continue;
+						}
+
+						correspondingExtensionMarkerType = extensionMarkerType;
+						break;
+					}
+
+					// An extension grouping type must include a satisfied extension marker type.
+					if (correspondingExtensionMarkerType is null)
+					{
+						continue;
+					}
+
+					// Okay, now we have got a satisfied extension grouping type.
+					result.Add(new(extensionGrouperType, correspondingExtensionMarkerType, @this));
+				}
+				return result.AsSpan();
+			}
+		}
+
+
 		/// <summary>
 		/// Try to enumerates for all possible extension containers that represents extension members for the current type.
 		/// </summary>
@@ -50,128 +163,24 @@ public static partial class ExtensionMemberLookup
 		/// A sequence of tuple of <see cref="Type"/> instances: extension grouper, extension marker
 		/// and the containing static class type.
 		/// </returns>
-		public IEnumerable<ExtensionContainerMetadata> GetExtensionContainers(params Assembly[]? assemblies)
+		public ReadOnlySpan<ExtensionContainerMetadata> GetExtensionContainers(params Assembly[]? assemblies)
 		{
-			// Iterate on each assembly.
+			var result = new List<ExtensionContainerMetadata>();
 			foreach (var assembly in assemblies is { Length: not 0 } ? assemblies : [Assembly.GetExecutingAssembly()])
 			{
-				// Check for all possible data types, no matter whether it satisfied or not.
 				foreach (var staticClassType in assembly.GetTypes())
 				{
-					// Step 1: Check for static class.
-
-					// This type must be 'static'.
-					if (!staticClassType.IsStatic)
+					foreach (var metadata in staticClassType.DeclaredExtensionContainers)
 					{
-						continue;
-					}
-
-					// This type must be marked as [Extension].
-					if (!staticClassType.MightBeExtension)
-					{
-						continue;
-					}
-
-					// Step 2: Check extension grouping type, whose name must be started
-					// with <see cref="ExtensionGrouperTypeNamePrefix"/>.
-					// This type is a static class.
-
-					// Iterate on all possible types defined in it.
-					foreach (var extensionGrouperType in staticClassType.GetMembers(DefaultBindingFlags).OfType<Type>())
-					{
-						// The name of this type must be started with <see cref="ExtensionGrouperTypeNamePrefix"/>.
-						if (!extensionGrouperType.Name.StartsWith(ExtensionGrouperTypeNamePrefix))
+						if (metadata.ExtensionMarker.GetMethod(ExtensionMarkerMethodName)!.Parameters[0].ParameterType == @this)
 						{
-							continue;
+							result.Add(metadata);
 						}
-
-						// This type must be 'sealed'.
-						if (!extensionGrouperType.IsSealed)
-						{
-							continue;
-						}
-
-						// This type must be marked as [Extension].
-						if (!extensionGrouperType.MightBeExtension)
-						{
-							continue;
-						}
-
-						// This type must be marked as [SpecialName].
-						// However, we should check for this type by using <see cref="Type.Attributes"/>,
-						// rather than using <see cref="MemberInfo.IsDefined(Type, bool)"/>.
-						if (!extensionGrouperType.IsSpecialName)
-						{
-							continue;
-						}
-
-						// This type must be non-'static'.
-						if (extensionGrouperType.IsStatic)
-						{
-							continue;
-						}
-
-						// Step 3: Check extension marker type defined in extension grouping type.
-
-						// This type is also a static class.
-						var correspondingExtensionMarkerType = default(Type);
-						foreach (var extensionMarkerType in extensionGrouperType.GetMembers(DefaultBindingFlags).OfType<Type>())
-						{
-							// The name of this type must be started with '<see cref="ExtensionMarkerTypeNamePrefix"/>'.
-							if (!extensionMarkerType.Name.StartsWith(ExtensionMarkerTypeNamePrefix))
-							{
-								continue;
-							}
-
-							// This type must be marked [SpecialName].
-							if (!extensionGrouperType.IsSpecialName)
-							{
-								continue;
-							}
-
-							// This type must be 'static'.
-							if (!extensionMarkerType.IsStatic)
-							{
-								continue;
-							}
-
-							// This type must contain one and only one member - a 'static' method,
-							// with name '<see cref="ExtensionMarkerMethodName"/>'.
-							var possibleMethodsInfo = extensionMarkerType.GetMembers(DefaultBindingFlags)
-								.OfType<MethodInfo>()
-								.ToArray();
-							if (possibleMethodsInfo is not [
-								{
-									IsGenericMethod: false,
-									Name: ExtensionMarkerMethodName,
-									ReturnType: var returnType,
-									IsSpecialName: true,
-									IsCompilerGenerated: true,
-									Parameters: [{ ParameterType: var parameterType }],
-								}]
-								|| parameterType != type
-								|| returnType != typeof(void))
-							{
-								continue;
-							}
-
-							correspondingExtensionMarkerType = extensionMarkerType;
-							break;
-						}
-
-						// An extension grouping type must include a satisfied extension marker type.
-						if (correspondingExtensionMarkerType is null)
-						{
-							continue;
-						}
-
-						// Okay, now we have got a satisfied extension grouping type.
-						yield return new(extensionGrouperType, correspondingExtensionMarkerType, staticClassType);
 					}
 				}
 			}
+			return result.AsSpan();
 		}
-
 
 		/// <summary>
 		/// Try to find for all possible members (properties, methods and operators) of the specified type,
@@ -183,9 +192,10 @@ public static partial class ExtensionMemberLookup
 		/// this method will defaultly find for extension members in the current-executing assembly.
 		/// </param>
 		/// <returns>All possible extension members found.</returns>
-		public IEnumerable<MemberInfo> FindExtensionMembers(ExtensionMemberTypes memberTypes, params Assembly[]? assemblies)
+		public ReadOnlySpan<MemberInfo> FindExtensionMembers(ExtensionMemberTypes memberTypes, params Assembly[]? assemblies)
 		{
-			foreach (var metadata in type.GetExtensionContainers(assemblies))
+			var result = new List<MemberInfo>();
+			foreach (var metadata in @this.GetExtensionContainers(assemblies))
 			{
 				foreach (var (callable, skeleton) in metadata.EnumerateExtensionMembers())
 				{
@@ -208,12 +218,13 @@ public static partial class ExtensionMemberLookup
 							when memberTypes.HasFlag(ExtensionMemberTypes.Operators)
 							&& methodName2.StartsWith("op_"):
 						{
-							yield return callable;
+							result.Add(callable);
 							break;
 						}
 					}
 				}
 			}
+			return result.AsSpan();
 		}
 	}
 }
